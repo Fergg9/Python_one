ee700e9b5eae3849f34271fd2560f713fc77d4c2
Fergg9 <jfrnando9@gmail.com>
2023-01-20 16:51:34 -0600

    mejoramos listas

--- lists.py
+++ lists.py
@@ -1,3 +1,13 @@
 numbers = [1, 2, 3, 4 ]
 habits = ['Run', 'study at platzi', 'get a cold shower']
 print(habits)
+print(habits[1])
+
+#sustituir un valor de una lista
+habits[0] = 'Walk with the dog'
+print(habits)
+
+palabra = 'CTRL+G guarda un documento nuevo de Word y actualiza uno existente.'
+numbers.append(palabra)
+print(numbers)
+print(numbers[-1])
