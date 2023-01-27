b6ff40df21924d15fba69a34d7cce78208008f51
Fergg9 <jfrnando9@gmail.com>
2023-01-27 14:38:38 -0600

    agregamos un ejemplo de la funcion map

---
+++ map.py
@@ -0,0 +1,10 @@
+numbers = (1,2,3,4)
+n = tuple(map(lambda num : num * 2, numbers ))
+print(numbers)
+print(n)
+
+xd = (57, 99, 1, 76, 43)
+lol = (5, 90, 23, 71, 89)
+sumita = tuple(map(lambda a, b: a + b, xd, lol))
+print(xd), print(lol)
+print(sumita)
